17cd2bc272911723691384bff3dcc84df3877f64
Elizaveta <ukrop@LAPTOP-T43LJSG0>
2024-02-25 09:49:27 +0200

    add form to add articles

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from hexlet_django_blog.article import views
+from hexlet_django_blog.article.views import IndexView, ArticleFormCreateView
 
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='articles_index'),
     # path('', views.index),
     path('<int:id>', views.ArticleView.as_view(), name='article_view'),
-    path('<int:article_id>/comments/<int:id>/', views.ArticleCommentsView.as_view()),
+    path('create/', ArticleFormCreateView.as_view(), name='articles_create')
 ]
--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -2,9 +2,17 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from django.views import View
 from django.urls import reverse, reverse_lazy
+from django.contrib import messages
+from django.contrib.messages import get_messages
 
 from hexlet_django_blog.article.models import Article
+from .forms import ArticleForm
 
+
+MESSAGE_TAGS = {
+    messages.INFO: "",
+    50: "critical",
+}
 
 # def index(request):
 #     return redirect(reverse('article',
@@ -34,12 +42,25 @@ class ArticleView(View):
         )
 
 
-class ArticleCommentsView(View):
+class ArticleFormCreateView(View):
 
     def get(self, request, *args, **kwargs):
-        comment = get_object_or_404(Comment, id=kwargs['id'], article_id=kwargs['article_id'])
+        form = ArticleForm()
         return render(
             request,
-            'articles/comments.html',
-            context={'comment': comment}
+            'articles/create.html',
+            {'form': form}
+        )
+    
+    def post(self, request, *args, **kwargs):
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/articles/')
+        messages.add_message(request, messages.INFO, "too short")
+        messages_list = get_messages(request)
+        return render(
+            request,
+            'articles/create.html',
+            {'form': form, 'messages': messages_list}
         )
